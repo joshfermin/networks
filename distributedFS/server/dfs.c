73351ba7d1222741db346fda0de9adae3b1468a8
joshfermin <joshfermin@gmail.com>
2015-10-21 20:46:49 -0600

    parsing config file on server end now works

--- distributedFS/server/dfs.c
+++ distributedFS/server/dfs.c
@@ -12,38 +12,70 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
+#include "configdfs.h"
+user *users;
+
 void parseConfFile(const char *);
+void processRequest(int);
+int countLines(const char *);
 
 void parseConfFile(const char *filename)
 {
-	char *line;
-    char head[64], tail[256];
+    char * line = NULL;
+    char * token;
     size_t len = 0;
     int read_len = 0;
-    char usernames[256], passwords[256];
+    int num_users = 0;
+    int i=0;
 
-    FILE* conf_file = fopen(filename, "r");
+    // open config file for reading
+    FILE *conf_file = fopen(filename, "r");
+    if (conf_file == NULL)
+        fprintf(stderr, "Could not open file.");
+
+    // get number of users
+    num_users = countLines(filename);
+    // printf("%d\n", num_users);
+    users = malloc(num_users * sizeof(user));
+
+    // for each line in config, add new user struct
     while((read_len = getline(&line, &len, conf_file)) != -1) {
-        // printf("%s",line);
-        line[read_len-1] = '\0';
-        if (line[0] == '#')
-            continue;
-        sscanf(line, "%s %s", head, tail);
-        strcat(usernames,head);	
-        strcat(passwords,tail);
-    }
-    printf("%s", usernames);
-    printf("\n%s",passwords);
+        token = strtok(line, " ");
+        strcpy(users[i].name, token);
 
+        token = strtok(NULL, " ");
+        strcpy(users[i].password,token);
+
+        // printf("Name: %s Password: %s\n",users[i].name, users[i].password);
+        i++;
+    }
     fclose(conf_file);
+}
+
+int countLines(const char *filename)
+{
+	FILE *fp = fopen(filename,"r");
+	int ch=0;
+	int lines=1;
+
+	while(!feof(fp))
+	{
+		ch = fgetc(fp);
+		if(ch == '\n')
+		{
+		lines++;
+		}
+	}
+	return lines;
+}
+
+void processRequest(int connection)
+{
+
 }
 
 int main(int argc, char *argv[], char **envp)
 {
-	// Display each command-line argument.
-   
-
-
-    parseConfFile("dfs.conf");
+    parseConfFile("server/dfs.conf");
     return 1;
 }
